72e6c1c30b9d5bbb27746fbe38d040a0210b2398
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2023-12-18 15:15:20 +0700

    Learn BE : fixing forget to add "one"

--- mongodb-mongoose/myApp.js
+++ mongodb-mongoose/myApp.js
@@ -71,7 +71,7 @@ const findPeopleByName = (personName, done) => {
 }
 
 const findOneByFood = (food, done) => {
-  Person.find({favoriteFoods: [food]}, function(err, data) {
+  Person.findOne({favoriteFoods: food}, function(err, data) {
     if (err) return console.error(err)
     done(null, data)
   })
