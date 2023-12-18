5a9e19f8a10b93cbc91c3e5289a8c1eae3b43347
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2023-12-18 15:13:11 +0700

    Learn BE : use model.findOne() to search in database

--- mongodb-mongoose/myApp.js
+++ mongodb-mongoose/myApp.js
@@ -71,7 +71,10 @@ const findPeopleByName = (personName, done) => {
 }
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/)
+  Person.find({favoriteFoods: [food]}, function(err, data) {
+    if (err) return console.error(err)
+    done(null, data)
+  })
 }
 
 const findPersonById = (personId, done) => {
