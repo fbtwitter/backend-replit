7d5937bf4f46545394536ff867b4fc5afe12ee15
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2023-12-28 12:47:42 +0700

    Learn BE : submit task on unix api

--- challenge-timestamp-microservice/index.js
+++ challenge-timestamp-microservice/index.js
@@ -2,31 +2,37 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+const cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 }))  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API New' })
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api/:unix?', function (req, res) {
+  const unix = +req.params?.unix;
+  let currentDate = new Date(unix)
 
+  res.json({ unix: unix, utc: currentDate })
+})
 
+const port = process.env.PORT || 3000
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
